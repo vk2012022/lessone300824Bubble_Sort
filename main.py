3992d072e4e2b707c91ba1c8308f3e4feeab69e7
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-09-01 10:37:34 +0300

    add new code 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 mas = [5, 7, 4, 3, 8, 2]
+n = 6
+for run in range(n-1):
+
+
+    for i in range(n - 1 - run):
+        if mas[i] > mas[i + 1]:
+            mas[i], mas[i + 1] = mas[i + 1], mas[i]
+
+print(mas)
